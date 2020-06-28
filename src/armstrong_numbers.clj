b919b666bb9e592afbdc1e655012d8ea9233f908
Agata Sumowska <agata.sumowska@gmail.com>
2020-06-28 19:03:54 +0200

    refactor

--- src/armstrong_numbers.clj
+++ src/armstrong_numbers.clj
@@ -3,22 +3,14 @@
 (defn exp [x n]
   (reduce * (repeat n x)))
 
-(defn getDigitFromNumber [s index]
-  (Integer/parseInt (str (nth s index)) ))
-
 (defn get-digits-from-number [n]
   (map #(Integer/parseInt (str %)) (seq (str n))))
 
-(defn expBasedOnBaseAsString [int-as-str index power]
-  (exp (getDigitFromNumber int-as-str index) power))
+(defn exp-int-list [list, power]
+  (map #(exp % power) list))
 
 (defn armstrong? [num]
     (= num 
-      (let [int-as-str (str num)
-            power (count int-as-str)]
-        (loop [ index 0
-                acc   0]
-          (if (= index power)
-            acc
-            (recur  (inc index) 
-                    (+ acc (expBasedOnBaseAsString int-as-str index power))))))))
+      (let [digits (get-digits-from-number num)
+            length (count digits)] 
+        (apply + (exp-int-list digits length)))))
